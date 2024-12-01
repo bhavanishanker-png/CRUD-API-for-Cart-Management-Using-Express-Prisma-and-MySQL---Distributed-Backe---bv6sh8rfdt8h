256361420d3a1899a8405d3fb4409464700fbbd6
bhavani shanker <bhavanishanker@bhavanis-MacBook-Pro.local>
2024-12-01 12:00:34 +0530

    done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,17 @@
-// middleware/authMiddleware.js
-const validApiKey = "8a60348b-d4a4-564a-9b45-aab518adb7f4";
+const authMiddleware = (req, res, next)=>{
+    const {apiauthkey}  = req.headers; 
+    const authKey = "8a60348b-d4a4-564a-9b45-aab518adb7f4"
+    if(!apiauthkey){
+        return res.status(401).json({ 
+            "message": "Access denied, apiauthkey is missing"
+         })
+    }
+    if(apiauthkey!=authKey){
+        return res.status(401).json({ 
+            "message": "Failed to authenticate apiauthkey"
+          })
+    }
+    next()
+}
 
-module.exports = (req, res, next) => {
-  const apiKey = req.header("apiauthkey");
-
-  if (!apiKey || apiKey !== validApiKey) {
-    return res.status(403).json({ error: "apiauthkey is missing or invalid" });
-  }
-  next();
-};
+module.exports = authMiddleware;
